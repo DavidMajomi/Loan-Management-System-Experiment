7be74832602f18c063cea86046ee276a33e883c0
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2023-09-07 23:51:37 -0400

    Preparing for statistics library development

---
+++ statisticsLibrary.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class BasicStatistics
+{
+    private:
+        vector <double> mode;
+        double mean, median, standardDeviation;
+    public:
+        BasicStatistics(/* args */);
+        ~BasicStatistics();
+};
+
+BasicStatistics::BasicStatistics(/* args */)
+{
+}
+
+BasicStatistics::~BasicStatistics()
+{
+}
