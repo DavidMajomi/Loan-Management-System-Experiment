552375818b1f705603d0d9c400340a2c251f15c1
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-01-15 11:33:20 -0500

    Code Cleanup and File Management

--- Header Files/constants.h
+++ Header Files/constants.h
@@ -17,34 +17,32 @@ const string OUTPUT_CSV_FILE_FOR_PROCESSED_DATA = CURRENT_FULL_PATH + "\\Csv Fil
 const string CSV_FILE_FOR_STORING_ANALAYZED_DB_VALUES = CURRENT_FULL_PATH + "\\Csv Files For Analysis\\analyzedDataFromDb.csv";
 const string CSV_FILE_FOR_TEMP_GENERATED_DATA_FOR_DB = CURRENT_FULL_PATH + "\\Folder with Generated Data\\tempGeneratedLoanDataForDbStorage.csv";
 
-// Best and Worst Values for Different Factors
+// Best Values for Different Factors
 const double BEST_DEFAULT_RISK_SCORE = 0; 
+const double WORST_DEFAULT_RISK_SCORE = 1;   // add this to loan viability formula
+
 const double BEST_LOSS_GIVEN_DEFAULT = 0;
-const double BEST_DEBT_TO_INCOME_RATIO = 0;
+const double WORST_LOSS_GIVEN_DEFAULT = 0.8;   // possible value includes 0.2, 0.1, 1 = too high
 
 // Worst Values for Different Factors
-const double WORST_DEFAULT_RISK_SCORE = 1;   // add this to loan viability formula
-const double WORST_LOSS_GIVEN_DEFAULT = 0.8;   // possible value includes 0.2, 0.1, 1 = too high
+const double BEST_DEBT_TO_INCOME_RATIO = 0;
 const double WORST_DEBT_TO_INCOME_RATIO = 0.9; //
 
 // Maximum and minimum Factor Values
+const double MIN_LOAN_AMOUNT = 500;
+const double MAX_LOAN_AMOUNT = 50000;
+
+const double MAX_FINANCIAL_RESERVES = 50000;
+const double MIN_FINANCIAL_RESERVES = 10000;
+
+const double MIN_LOAN_DURATION = 1;
+const unsigned short int MAX_LOAN_DURATION = 60;
+
 const unsigned short int MAX_CREDIT_SCORE = 850;
 const unsigned short int MIN_CREDIT_SCORE = 300;
 
 const unsigned short int MIN_MONTHLY_INCOME = 800;
 const unsigned short int MAX_MONTHLY_INCOME = 12000;
-
-const double MIN_LOAN_DURATION = 1;
-const unsigned short int MAX_LOAN_DURATION = 60;
-
-const double MAX_FINANCIAL_RESERVES = 50000;
-const double MIN_FINANCIAL_RESERVES = 10000;
-
-const double MIN_LOAN_AMOUNT = 500;
-const double MAX_LOAN_AMOUNT = 50000;
-
-const double MIN_LOAN_DURATION = 1;
-const unsigned short int MAX_LOAN_DURATION = 60;
 
 
 // Weights of Different Factors
