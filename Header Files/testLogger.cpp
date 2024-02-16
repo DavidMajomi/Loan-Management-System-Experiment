0fc3d67b346d256a33611649b227787884ec7120
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-02-15 23:06:04 -0500

    Update testLogger.cpp

--- Header Files/testLogger.cpp
+++ Header Files/testLogger.cpp
@@ -1,5 +1,7 @@
 #include "Logger.h"
 
+using std::to_string;
+
 Logger display;
 
 int main()
@@ -11,10 +13,11 @@ int main()
     display.switchScreenDisplay();
     display.output("This statemnt should not be visible.");
     display.switchScreenDisplay();
-    display.outputWithMoreDetails(__FUNCTION__, "test", "bool",to_string(__LINE__), to_string(test), "No comments");
+    display.outputWithMoreDetails(__FUNCTION__, test, "test", to_string(__LINE__), "No comments");
 
     display << "This an operator overloading test";
-    // display << "This an operator overloading test";
+    display << (1234);
+    display.display("here we are in the zone", "testing the mic", 1234, "\nThe logger is working");
 
     return 0;
 }
