f6926fe267b29d56cfbc7b4b357c9e3b2b9c4a36
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-03-09 02:43:45 -0500

    Update databaseAbstraction.h

--- Header Files/databaseAbstraction.h
+++ Header Files/databaseAbstraction.h
@@ -161,7 +161,6 @@ namespace databaseAbstraction
     }
 
 
-
     bool validateStringIsDigit(string value)
     {
         bool validNumber = true;
@@ -479,5 +478,69 @@ namespace databaseAbstraction
     }
 
 
+    double storeSingleRowInDbUsingSingleInsert(const char * databaseFullPath, string sqlInsertFormat, string sqlInsertTableNames, string singleValueToInsert)
+    {
+        clock_t time;
+
+        time = clock();
+
+        sqlite3* db;
+        int numberOFInsertions;
+        int rc = sqlite3_open(databaseFullPath, &db);
+        char charFinalSqlInsertStatement;
+        double timeDouble;
+        const char* sqlInsertLine;
+        char * sqliteErrorMessage;
+        string stringSql = "BEGIN TRANSACTION; " + sqlInsertFormat;
+        const char* sql = stringSql.c_str();
+        vector <string> allInsertStatements;
+        string insertToSql, stringFinalSqlInsertStatement, completedSqlStatement;
+        sqlite3_stmt* stmt;
+
+        if (rc != SQLITE_OK)
+        {
+            throw " FAILURE OPENING DATABASE";
+        }
+        else
+        {
+            rc = sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr);
+
+            if (rc != SQLITE_OK) {
+                sqlite3_close(db);
+                throw " FAILURE COMPILING SQL STATEMENT";
+            }
+            rc = sqlite3_exec(db, sql, 0, 0, &sqliteErrorMessage);
+
+            if (rc != SQLITE_OK) {
+                sqlite3_close(db);
+                throw sqliteErrorMessage;
+            }
+
+
+            completedSqlStatement = singleValueToInsert + "COMMIT;";
+
+            sqlInsertLine = completedSqlStatement.c_str();
+            sql = sqlInsertLine;
+
+            // cout << endl << sql <<endl;
+            rc = sqlite3_exec(db, sql, 0, 0, &sqliteErrorMessage);
+
+            if (rc != SQLITE_OK) {
+                throw sqliteErrorMessage;
+                sqlite3_close(db);
+            }
+
+            sqlite3_finalize(stmt);
+            sqlite3_close(db);
+
+            time = clock() - time;
+
+            timeDouble = (double)(time);
+            
+        }
+
+        return timeDouble;
+
+    }
     
 }
