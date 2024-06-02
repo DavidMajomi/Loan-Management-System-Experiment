f129e0ba2e4ba4b617e42c800abb0c6ddb9552dc
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-06-01 22:44:50 -0400

    Update analysis_experiment.py

--- Src/analysis_experiment.py
+++ Src/analysis_experiment.py
@@ -70,6 +70,108 @@ from sklearn.linear_model import LogisticRegression
 super_prime_rate = 8.33
 deep_sub_prime_rate = 30.00
 
+
+
+    
+df = pd.read_csv("../Csv Files For Analysis/analyzedDataFromDb.csv")
+df = df.dropna()
+df = df.drop(columns=['Name'])
+# print(df)
+
+def predictYearlyInterestRateByGroup(credit_score):
+        
+    columns = ["credit_score"]
+    x = df[columns]
+    y = df['interest_rate_by_group']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
+
+    model = DecisionTreeRegressor()
+
+    model.fit(X_train.values, y_train.values)
+
+    predictions = model.predict(X_test)
+
+    print("Mean Squared error: ", mean_squared_error(y_test, predictions))
+    print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
+    
+    return float(model.predict([[credit_score]]))
+
+
+def predictALVS(credit_score, monthly_income, financial_reserves, 
+                                                          debt_to_income_ratio, Duration_in_months, 
+                                                          loan_amount_requested, 
+                                                          loss_Given_Default, 
+                                                          defult_risk_score):
+    
+    columns = ["credit_score", "monthly_income", "financial_reserves", "debt_to_income_ratio", "Duration_in_months", "loan_amount_requested",
+                                    "loss_Given_Default", "default_risk_score"]
+    x = df[columns]
+    y = df['Adjusted_Loan_viability_Score']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
+
+    model = LinearRegression()
+
+    model.fit(X_train.values, y_train.values)
+
+    predictions = model.predict(X_test)
+
+    # print("Mean Squared error: ", mean_squared_error(y_test, predictions))
+    # print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
+    
+    return float(model.predict([[credit_score, monthly_income, financial_reserves, 
+                                                          debt_to_income_ratio, Duration_in_months, 
+                                                          loan_amount_requested, 
+                                                          loss_Given_Default, 
+                                                          defult_risk_score]])), model
+
+
+def predictDefaultRiskScore(monthly_interest_rate_by_group, super_prime_rate, deep_sub_prime_rate):
+    columns = ["interest_rate_by_group"]
+    # columns = ["interest_rate_by_group", "best_possible_rate", "worst_possible_rate"]
+
+    x = df[columns]
+    x = x.div(12)
+    y = df['default_risk_score']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
+
+    modelRate = LinearRegression()
+
+    modelRate.fit(X_train.values, y_train.values)
+
+    predictions = modelRate.predict(X_test)
+
+    print("Mean Squared error: ", mean_squared_error(y_test, predictions))
+    print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
+    
+    # default_risk_score = float(modelRate.predict([[monthly_interest_rate_by_group, super_prime_rate, deep_sub_prime_rate]]))
+    default_risk_score = float(modelRate.predict([[monthly_interest_rate_by_group]]))
+    
+    # return default_risk_score, modelRate
+    return monthly_interest_rate_by_group - ((super_prime_rate - 3) / 12), modelRate
+
+
+def predictInterestRateFromAlvs(alvs):
+    columns = ['Adjusted_Loan_viability_Score']
+    x = df[columns]
+    y = df['Interest_rate_over_a_year']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
+
+    model = LinearRegression()
+
+    model.fit(X_train.values, y_train.values)
+
+    predictions = model.predict(X_test)
+
+    print("Mean Squared error: ", mean_squared_error(y_test, predictions))
+    print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
+    
+    return float(model.predict([[alvs]])), model
+  
+  
 credit_score = 850
 monthly_income = 12000
 financial_reserves = 50000
@@ -78,95 +180,45 @@ Duration_in_months = 1
 loan_amount_requested = 20000
 
 
-    
-df = pd.read_csv("../Csv Files For Analysis/analyzedDataFromDb.csv")
-df = df.dropna()
-print(df)
-
-
-
-################################################################
-# Predict interest rate by group
-
-columns = ["credit_score"]
-x = df[columns]
-y = df['interest_rate_by_group']
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
-
-model = DecisionTreeRegressor()
-
-model.fit(X_train.values, y_train.values)
-
-predictions = model.predict(X_test)
-
-print("Mean Squared error: ", mean_squared_error(y_test, predictions))
-print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
-
-credit_score = 850
-interestRateByGroup = float(model.predict([[credit_score]]))
-print(interestRateByGroup, " ", type(interestRateByGroup))
-
-
-
-slope = (deep_sub_prime_rate - super_prime_rate) / -100 
-
-
-
-columns = ["credit_score", "monthly_income", "financial_reserves", "debt_to_income_ratio", "Duration_in_months", "loan_amount_requested",
-                                "loss_Given_Default", "default_risk_score"]
-x = df[columns]
-y = df['Adjusted_Loan_viability_Score']
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
-
-model = LinearRegression()
-
-model.fit(X_train.values, y_train.values)
-
-predictions = model.predict(X_test)
-
-print("Mean Squared error: ", mean_squared_error(y_test, predictions))
-print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
-
 # credit_score = 350
 # monthly_income = 2000
 # financial_reserves = 1084.64
 # debt_to_income_ratio = 0.42
 # Duration_in_months = 13
 # loan_amount_requested = 9413
+  
+interestRateByGroup = predictYearlyInterestRateByGroup(credit_score)
 
-
-columns = ["interest_rate_by_group"]
-
-x = df[columns]
-y = df['default_risk_score']
-
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101) 
-
-modelRate = LinearRegression()
-
-modelRate.fit(X_train.values, y_train.values)
-
-predictions = modelRate.predict(X_test)
-
-print("Mean Squared error: ", mean_squared_error(y_test, predictions))
-print("Mean absolute error: ", mean_absolute_error(y_test, predictions))
     
 monthly_interest_rate_by_group = interestRateByGroup / 12
     
-defult_risk_score = monthly_interest_rate_by_group - (super_prime_rate / 12)
+# defult_risk_score = monthly_interest_rate_by_group - (super_prime_rate / 12)
+print(monthly_interest_rate_by_group - (super_prime_rate / 12))
+print(super_prime_rate)
+print("_____________________")
+defult_risk_score, defaulr_risk_model = predictDefaultRiskScore(monthly_interest_rate_by_group, super_prime_rate, deep_sub_prime_rate)
 
 loss_Given_Default = (loan_amount_requested - financial_reserves) / loan_amount_requested; # SOURCE = WIKIPEDIA
 
-adjusted_loan_viability_score = float(model.predict([[credit_score, monthly_income, financial_reserves, debt_to_income_ratio, Duration_in_months, loan_amount_requested, loss_Given_Default, defult_risk_score]]))
 
+adjusted_loan_viability_score, alvsModel = predictALVS(credit_score, monthly_income, financial_reserves, 
+                                                          debt_to_income_ratio, Duration_in_months, 
+                                                          loan_amount_requested, 
+                                                          loss_Given_Default, 
+                                                          defult_risk_score)
 
-normal_monthly_interest_rate = (slope * adjusted_loan_viability_score) + deep_sub_prime_rate
+# normal_monthly_interest_rate = (slope * adjusted_loan_viability_score) + deep_sub_prime_rate
+normal_yearly_interest_rate, yearlyInterestRateModel = predictInterestRateFromAlvs(adjusted_loan_viability_score)
+
 
 
 print(adjusted_loan_viability_score)
-print(f" This is normal rate: {normal_monthly_interest_rate} ")
+print(f"This is interest rate by group: {interestRateByGroup}")
+print(f"This is monthly interest rate by group: {monthly_interest_rate_by_group}")
+print(f"This is  ")
+print(f"This is ")
+print(f"This is ")
+print(f" This is normal rate: {normal_yearly_interest_rate} ")
 print(f" This is default risk score: {defult_risk_score} ")
 
 
@@ -178,7 +230,7 @@ theTree = theTree.fit(score.values, decision.values)
 
 print("Loan Decision: ", theTree.predict([[adjusted_loan_viability_score]]))
 
-list_of_models = [model, theTree]
+list_of_models = [alvsModel, theTree]
 
 with open("LMSModel.pickle", "wb") as file:
     pickle.dump(list_of_models, file)
