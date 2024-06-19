b57e9fd7fffba37a434bddbde26c59201c1f921a
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-06-19 09:54:04 -0400

    Update testTime.cpp

--- Header Files/testTime.cpp
+++ Header Files/testTime.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main()
 {
-    string value = "Sat Jun 15 16:46:06 2024\n";
+    // string value = "Sat Jun 15 16:46:06 2024\n";
+    string value = "Mon Jan 18 15:50:49 2027\n";
     time_t now = time(0);
     time_t rawtime;
     time_t bingeTime;
@@ -39,10 +40,23 @@ int main()
     // cout << timeManip::getDateAsStr(&oneday);
 
     time_t fake = timeManip::createDateFromString(value);
+    cout << "_________" << endl;
     cout << value;
     cout << ctime(&fake);
+    // cout 
     time_t check = timeManip::createDateFromStringTimeIrrelevant(value);
     cout << ctime(&check);
+    cout << timeManip::getDateAsStr(&check) << endl;
+
+    cout << timeManip::convSecsToDays(86400.00) << endl;
+    cout << timeManip::convSecsToHours(86400.0) << endl;
+    cout << timeManip::convSecsToHours(5000.00) << endl;
+    cout << timeManip::convSecsToMonths(2629746) << endl;
+
+    time_t nowAgain = timeManip::getLocalTimeStruct();
+
+    cout << ctime(&nowAgain);
+    cout << timeManip::getLocalTimeStr();
 
     
 
