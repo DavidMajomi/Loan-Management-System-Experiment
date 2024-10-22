c9934003ab36450aaf9ddea4223c4d33fd857b32
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-10-22 10:35:10 -0400

    Update testTime.cpp

--- Header Files/testTime.cpp
+++ Header Files/testTime.cpp
@@ -8,23 +8,26 @@ using namespace std;
 int main()
 {
     // string value = "Sat Jun 15 16:46:06 2024\n";
-    string value = "Mon Jan 18 15:50:49 2027\n";
+    string value = "Mon Jan 18 15:50:49 2027";
+    time_t check = timeManip::createDateFromStringTimeIrrelevant(value);
+    string check_str = ctime(&check);
 
     time_t now = time(0);
 
-    cout << "Num seconds since unix time thingy = " << now << endl << endl;
+    // cout << "Num seconds since unix time thingy = " << now << endl << endl;
 
     tm * localTime = localtime(&now);
     time_t locTime = mktime(localTime);
     string currLocTime = ctime(&locTime);
     
-    cout << "Current Time:" << currLocTime;
+    cout << "Current Time: " << currLocTime;
+    // string twelve_months_from_now 
     cout << "String Date 12 months from now: " << timeManip::getDateNMonthsFromDateStr(localTime, 13);
     
     cout << "Current Date and Time using Library: " << timeManip::getDateAsStr(&now);
     assert(currLocTime == timeManip::getDateAsStr(&now));
 
-    
+
     cout << "12 months from now: " << timeManip::getDateNDaysFromDateStr(now, 365);
     
     time_t fake = timeManip::createDateFromString(value);
@@ -32,7 +35,9 @@ int main()
     cout << value;
     cout << ctime(&fake);
 
-    time_t check = timeManip::createDateFromStringTimeIrrelevant(value);
+    cout << "Checking conversion of " << value << " to Mon Jan 18 00:00:00 2027\n" << endl;
+    assert(check_str == "Mon Jan 18 00:00:00 2027\n");
+
     cout << ctime(&check);
     cout << "__________________________" << endl;
     cout << endl;
@@ -46,7 +51,7 @@ int main()
 
     time_t nowAgain = timeManip::getLocalTimeStruct();
 
-    cout << ctime(&nowAgain);
+    cout << "Local Time Struct: " << ctime(&nowAgain);
     
     string currTime = timeManip::getLocalTimeStr();
     
